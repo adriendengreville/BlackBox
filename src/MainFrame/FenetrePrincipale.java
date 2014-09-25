a6de9ff52be60130e47bc7ffc74282c387488616
didien <didienv2@gmail.com>
2014-09-25 17:16:02 +0200

    Ajout UI

--- src/MainFrame/FenetrePrincipale.java
+++ src/MainFrame/FenetrePrincipale.java
@@ -6,6 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.UIManager;
+
+import java.awt.GridLayout;
+
+import javax.swing.JTabbedPane;
+import java.awt.Window.Type;
 
 public class FenetrePrincipale extends JFrame {
 
@@ -31,12 +40,47 @@ public class FenetrePrincipale extends JFrame {
 	 * Create the frame.
 	 */
 	public FenetrePrincipale() {
+		try
+		{
+		  UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception e){}
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("Param\u00E8tres");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmPseudo = new JMenuItem("Pseudo");
+		mnNewMenu.add(mntmPseudo);
+		
+		JMenuItem mntmQuitter = new JMenuItem("Quitter");
+		mnNewMenu.add(mntmQuitter);
+		
+		JMenu mnRseau = new JMenu("R\u00E9seau");
+		menuBar.add(mnRseau);
+		
+		JMenuItem mntmRejoindreUnRseau = new JMenuItem("Rejoindre un r\u00E9seau");
+		mnRseau.add(mntmRejoindreUnRseau);
+		
+		JMenuItem mntmQuitterLeRseau = new JMenuItem("Quitter le r\u00E9seau");
+		mnRseau.add(mntmQuitterLeRseau);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("Client", null, panel, null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("Serveur", null, panel_1, null);
 	}
 
 }
