29057f0efa7e22ea080cfea6fc0733a53feba7b5
didien <didienv2@gmail.com>
2014-10-09 20:18:14 +0200

    Update crypto

--- BlackBox/src/mainFrame/Cryptage.java
+++ BlackBox/src/mainFrame/Cryptage.java
@@ -8,20 +8,29 @@ import java.util.Vector;
 
 public class Cryptage {
 	//ATTRIBUTS
-		int privateKey;
-		int publicKey;
+		BigInteger privateKey;
+		BigInteger publicKey;
+		String messageTest = "Bonjour je suis une tulipe codée";
+		Vector<Integer> tableCodee;
 		
 		Vector<Integer> tablePr = new Vector<Integer>();
 	
 	//MÉTHODES
-		public int encrypt(int publicKey) {
-			// TODO Auto-generated method stub
-			return publicKey;
+		
+		public Cryptage(){
+			genererPremier(); 	//on etablit la liste des nombres premiers
+		}
+		
+		public Vector<Integer> encrypt(BigInteger publicKey, String message) {
+			for (int i = 0; i < message.length(); ++i){
+				
+			}
+			return null;
 		}
 	
-		public int decrypt(int privateKey) {
+		public String decrypt(BigInteger privateKey) {
 			// TODO Auto-generated method stub
-			return privateKey;
+			return null;
 		}
 	
 		public void computeRSA_Key() {
@@ -31,8 +40,10 @@ public class Cryptage {
 			BigInteger q;
 			BigInteger n;
 			BigInteger phi;
+			BigInteger d;
+			BigInteger e;
 			
-			genererPremier();		//on etabli la liste de nombres premiers
+			//déplacement de la création de la liste dans le constructeur
 			
 			p1 = tablePr.get((int) (Math.random() * tablePr.size()));
 			
@@ -40,19 +51,34 @@ public class Cryptage {
 				q1 = tablePr.get((int) (Math.random() * tablePr.size()));
 			}while (p1 > q1);
 			
+			//nombres premiers choisis
 			p = new BigInteger(p1.toString());
 			q = new BigInteger(q1.toString());
 			
 			n 	= p.multiply(q);
-			phi = (p.subtract(new BigInteger("1"))).multiply((q.subtract(new BigInteger("1"))));
+			phi = (p.subtract(new BigInteger("1"))).multiply((q.subtract(new BigInteger("1"))));	//phi = (p-1) * (q-1)
+			
+			if (PGCD(p, q) == 1){
+				System.out.println(p + " et " + q + " sont premiers entre eux.");
+			}
 			
 			System.out.println("p = " + p1 + "\n"+
 					"q = " + q1 + "\n"+
 					"n = " + n + "\n"+
 					"phi = " + phi + "\n");
-		}
+			
+			d = new BigInteger(q.add(new BigInteger("1")).toString());	// d fait parti du couple de clé privée 
+			while(PGCD(phi, d) != 1)	//on augmente de un d jusqu'à ce qu'il soit premier avec phi
+				d = d.add(new BigInteger("1"));
+			
+			System.out.println("d = " + d);
+			
+			e = new BigInteger(d.modInverse(phi).toString());	//e fait parti du couple de clés publiques et vaut d^-1 mod phi 
+			
+			System.out.println("e = " + e);
+		}//computeRSA_Key
 		
-		private void genererPremier() {
+		private void genererPremier() {		//genere 20 000 nombres premiers en une seconde
 			int a = 0;
 			int b = 0;
 			int pr;
@@ -78,6 +104,7 @@ public class Cryptage {
 			}
 		}//genererPremier
 		
+		@SuppressWarnings("unused")
 		private boolean isPremier(int number){
 			int nb 		 = number;
 			int compter  = 0;
@@ -99,22 +126,40 @@ public class Cryptage {
 				System.out.println(nb + " n'est pas premier.");
 			
 			return !test;
-		}
+		}//isPremier
+		
+		private int PGCD(BigInteger a, BigInteger b){
+			int res;
+			
+			while(! a.multiply(b).equals(new BigInteger("0"))){
+				res = a.compareTo(b);
+				if (res == 1)	//si a > b alors res = 1
+					a = a.subtract(b);
+				else
+					b = b.subtract(a);
+			}
+			
+			res = a.compareTo(new BigInteger("0"));
+			if (res == 0)
+				return b.intValue();
+			else
+				return a.intValue();
+		}//PGCD
 		
 	//SET-GETTER
-		public void setPrivateKey(int privateKey) {
+		public void setPrivateKey(BigInteger privateKey) {
 			this.privateKey = privateKey;
 		}
 	
-		public int getPrivateKey() {
+		public BigInteger getPrivateKey() {
 			return this.privateKey;
 		}
 	
-		public void setPublicKey(int publicKey) {
+		public void setPublicKey(BigInteger publicKey) {
 			this.publicKey = publicKey;
 		}
 	
-		public int getPublicKey() {
+		public BigInteger getPublicKey() {
 			return this.publicKey;
 		}
 		
